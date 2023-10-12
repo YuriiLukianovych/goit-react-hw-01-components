4cbf7456bbb65687695536093eae0f80ee10da5c
Yurii Lukianovych <mr.lukyanovich@gmail.com>
2023-10-12 17:51:55 +0300

    error fixed

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,31 +1,29 @@
 import css from './TransactionHistory.module.scss';
 
 const TransactionHistory = ({ items }) => {
-  {
-    return (
-      <table className={css.transactionHistory}>
-        <thead>
-          <tr>
-            <th>Type</th>
-            <th>Amount</th>
-            <th>Currency</th>
-          </tr>
-        </thead>
+  return (
+    <table className={css.transactionHistory}>
+      <thead>
+        <tr>
+          <th>Type</th>
+          <th>Amount</th>
+          <th>Currency</th>
+        </tr>
+      </thead>
 
-        <tbody>
-          {items.map(el => {
-            return (
-              <tr key={el.id}>
-                <td>{el.type}</td>
-                <td>{el.amount}</td>
-                <td>{el.currency}</td>
-              </tr>
-            );
-          })}
-        </tbody>
-      </table>
-    );
-  }
+      <tbody>
+        {items.map(el => {
+          return (
+            <tr key={el.id}>
+              <td>{el.type}</td>
+              <td>{el.amount}</td>
+              <td>{el.currency}</td>
+            </tr>
+          );
+        })}
+      </tbody>
+    </table>
+  );
 };
 
 export default TransactionHistory;
